770b28275f8f4d66a0ad6b9ab3140388e777d9a5
Fernando.Ocion <fernando.ocion@gmail.com>
2024-05-08 02:16:13 -0300

    Estudo de Abstração

---
+++ Abstração/ComputadorFernando.java
@@ -0,0 +1,24 @@
+package Abstração;
+
+public class ComputadorFernando {
+	public static void main(String[] args) {
+		
+        System.out.println("MSN");
+		MSNMessenger msn = new MSNMessenger();
+		msn.enviarMensagem();
+		msn.receberMensagem();
+		
+        System.out.println("\nFACEBOOK");
+
+		FacebookMessenger fbm = new FacebookMessenger();
+		fbm.enviarMensagem();
+		fbm.receberMensagem();
+		
+        System.out.println("\nTELEGRAM");
+
+		Telegram tlg = new Telegram();
+		tlg.enviarMensagem();
+		tlg.receberMensagem();
+		
+	}
+}
