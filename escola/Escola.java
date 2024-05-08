5495a3b744b8288b3ff18cd8fbaff5d66072c8e8
Fernando.Ocion <fernando.ocion@gmail.com>
2024-05-08 02:15:28 -0300

    Alteração Escola

--- escola/Escola.java
+++ escola/Escola.java
@@ -2,10 +2,10 @@ package escola;
 
 public class Escola {
 	public static void main(String[] args) {
-		Aluno felipe = new Aluno();
-		felipe.setName("Fernando");
-		felipe.setIdade(33);
+		Aluno fernando = new Aluno();
+		fernando.setName("Fernando");
+		fernando.setIdade(33);
 		
-		System.out.println("O aluno " + felipe.getName() + " tem " + felipe.getIdade() + " anos ");	
+		System.out.println("O aluno " + fernando.getName() + " tem " + fernando.getIdade() + " anos ");	
 	}
 }
